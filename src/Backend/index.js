97faf0597d99576576dbe7eb56024722aa2b02b2
kombate1 <bikinankombate1@gmail.com>
2024-12-14 18:58:08 +0000

    Added new feature to menu

--- src/Backend/index.js
+++
@@ -1,7 +0,0 @@
-const { ObjectId } = require('mongodb');
-
-// Example of how you might be using ObjectId incorrectly
-// let id = ObjectId(someId); // Incorrect usage
-
-// Correct usage
-let id = new ObjectId(someId); // Ensure 'new' is used 

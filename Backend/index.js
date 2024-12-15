bd3433e63fa161db450fb7068e8fdcb07f736605
kombate1 <bikinankombate1@gmail.com>
2024-12-15 11:54:06 +0000

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,18 @@
 import express from 'express';
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 const app = express();
 app.use(bodyParser.json());
 
-// Use CORS middleware
+// Get the directory name from import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Serve static files from the dist directory
+// Serve static files from the "dist" folder
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all route to serve index.html for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
 
 app.use(
